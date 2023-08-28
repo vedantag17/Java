d0753f015c625ee56f975cbbe10c016867763aba
Vedant Agrawal <vrsagrawal@gmail.com>
2023-08-28 23:33:46 +0530

    Initial commit

---
+++ src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner inputs = new Scanner(System.in);
+        System.out.println("Please Enter some input:");
+        int rollno = inputs.nextInt();
+        System.out.println("Your rollno is:"+ rollno);
+    }
+}
---
+++ src/Primitives.java
@@ -0,0 +1,10 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 1;
+        char letter = 'd';
+        float number= 5670.97f;
+        double largeFloatNumber = 56798.978;
+        long largeInteger = 23423232424L;
+        boolean check = true;
+    }
+}
