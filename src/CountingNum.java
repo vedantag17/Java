ab11281ca661a237c3e6f867030b98b307d4062c
Vedant Agrawal <vrsagrawal@gmail.com>
2023-09-18 22:37:36 +0530

    Counting same digit available in that number

---
+++ src/CountingNum.java
@@ -0,0 +1,14 @@
+public class CountingNum {
+    public static void main(String[] args) {
+        int n = 55555555;
+        int count = 0;
+        while (n > 0) {
+            int rem = n %10;
+            if(rem == 5) {
+                count++;
+            }
+            n = n/10;
+        }
+        System.out.println(count);
+    }
+}
